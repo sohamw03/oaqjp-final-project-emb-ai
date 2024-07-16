5b29f3c02d7c1649e30e80ccb94a4f230c871713
Soham Waghmare <102748742+sohamw03@users.noreply.github.com>
2024-07-16 11:39:18 -0400

    feat: task 7

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -5,6 +5,15 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     payload = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, headers=headers, json=payload)
+    if response.status_code == 400:
+        return {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None
+        }
     response_dict = json.loads(response.text)
     emotion_scores = {
         "anger": response_dict["emotionPredictions"][0]["emotion"]["anger"],
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
+import json
 
 app = Flask(__name__) 
 
@@ -11,6 +12,8 @@ def home():
 def sentiment_analysis():
     textToAnalyze = request.args.get("textToAnalyze")
     result = emotion_detector(textToAnalyze)
+    if result["dominant_emotion"] == None:
+        return "Invalid text! Please try again!"
     return result
 
 if __name__ == "__main__":
